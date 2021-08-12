59e152e14bfa3e5f06573c1ffa8c77f896c881f1
Christopher <c.barnhurst@live.com>
2021-08-12 18:49:36 -0500

    commit tree 70fbfe871096b13018b66b0f9326fd194874eb10

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Add your code here
+const submitData= function(userName, userEmail) {
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body:JSON.stringify({
+            "name":userName,
+            "email":userEmail
+        })
+    }).then(response=> response.json())
+    .then(data=>renderId(data))
+    .catch((error)=> {
+        const span=document.createElement("span")
+        span.innerHTML=error.message;
+        document.body.append(span)
+})
+}
+const renderId=data=> {
+    const span=document.createElement("span")
+    span.innerHTML=data.id;
+    document.body.append(span)
+}
+
+
+// const submitData = {
+//     userName: "name",
+//     userEmail: "email"
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/users", configurationObject)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     }); 
+  
+ 
